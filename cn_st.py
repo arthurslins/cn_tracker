10626f84ce22963c9d064d30fe23e5e537f051ce
Arthur Lins <arthurlins@id.uff.br>
2022-07-01 01:57:05 -0300

    Update cn_st.py

--- cn_st.py
+++ cn_st.py
@@ -3,6 +3,13 @@ import pandas as pd
 import numpy as np
 import requests
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 df_cn=pd.read_csv('df_cn.csv')
