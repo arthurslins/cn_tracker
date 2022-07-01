d7b8ae8074ea41c62c97e18e1c677e6bfec843be
Arthur Lins <arthurlins@id.uff.br>
2022-07-01 02:50:25 -0300

    Update cn_st.py

--- cn_st.py
+++ cn_st.py
@@ -28,7 +28,7 @@ if st.button('Clica para atualizar sa porra'):
 
     urls.append(driver.current_url)
     driver.back()
-    df_cn=pd.DataFrame([],columns=['Nick','PDL','Jogos','link])
+    df_cn=pd.DataFrame([],columns=['Nick','PDL','Jogos','link'])
     for i in range(0,20):
         cn_list=driver.find_element('xpath','//*[@id="appMain"]/div[1]/div/div[2]/div[2]')
         cn_list2=cn_list.text.split('%\n')
