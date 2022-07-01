f34018988d7c2b2cd27f8f15f0fc9257c71739cc
Arthur Lins <arthurlins@id.uff.br>
2022-07-01 11:55:12 -0300

    m

--- cn_st.py
+++ cn_st.py
@@ -18,6 +18,7 @@ firefoxOptions.add_argument("--headless")
 
 df_cn=pd.read_csv('df_cn.csv')
 df_cn2=pd.read_csv('parcial.csv')
+df_cn2.drop('Unnamed: 0',axis=1,inplace=True)
 st.write(df_cn2,unsafe_allow_html=True)
 
 if st.button('Clica para atualizar sa porra'):
