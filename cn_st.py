fc50535b279283f025830a87dbd1c6d7ff738ce8
Arthur Lins <arthurlins@id.uff.br>
2022-07-01 03:05:50 -0300

    Update cn_st.py

--- cn_st.py
+++ cn_st.py
@@ -20,7 +20,8 @@ df_cn=pd.read_csv('df_cn.csv')
 
 if st.button('Clica para atualizar sa porra'):
     st.write('espera um cado fião') 
-    driver = webdriver.Firefox(firefox_options=opts)
+    driver = webdriver.Firefox(options=firefoxOptions,
+    service=service)
    
     driver.get("https://lol.qq.com/tft/#/rank/tier")
     driver.implicitly_wait(20)
