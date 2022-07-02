984644d0727e6d1aad40a239c9393346d3971239
Arthur Lins <arthurlins@id.uff.br>
2022-07-02 12:06:37 -0300

    aa

--- cn_st.py
+++ cn_st.py
@@ -44,8 +44,8 @@ if st.button('Clica para atualizar sa porra'):
             cn_list3.append(cn_list2[j].split('\n'))
 
         for k in range(0,10):
-            # st.write(k)
-            # st.write(cn_list3)
+            st.write(k)
+            st.write(cn_list3)
             if cn_list3[k][0].isdigit():
                 del cn_list3[k][0]
 
