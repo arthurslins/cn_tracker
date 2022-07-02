5919f6913e927771216638d6eae84f006401cf9a
Arthur Lins <arthurlins@id.uff.br>
2022-07-02 03:43:39 -0300

    aa

--- cn_st.py
+++ cn_st.py
@@ -34,8 +34,8 @@ if st.button('Clica para atualizar sa porra'):
     
     df_cn=pd.DataFrame([],columns=['Nick','PDL','Jogos','link'])
     
-    for pagi in range(0,10):
-        st.write(pagi)
+    for _ in range(10):
+        
         cn_list=driver.find_elements(by=By.CLASS_NAME,value='ranking-body')
         cn_list2=cn_list[0].text.split('%\n')
         del cn_list2[1::2]
